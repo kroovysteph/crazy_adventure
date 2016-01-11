215713fa53c47955c63179275f2f956106c1ebff
kroovysteph <kroovy@web.de>
2016-01-11 14:56:43 +0100

    now does removed adventure before compiling

--- linux_runscript.sh
+++ linux_runscript.sh
@@ -4,4 +4,10 @@ cd prog1lib/lib/;
 make;
 cd ../../;
 echo ">> prog1lib successfully compiled!";
+
+if [[ -s ./adventure ]] ; then
+	rm adventure;
+	echo "wurde geloescht";
+fi
+
 make adventure && ./adventure;
