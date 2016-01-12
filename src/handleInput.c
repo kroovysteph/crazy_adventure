9de83c3e571d8f8e5a01ea5025214b827557cbd1
kroovysteph <kroovy@web.de>
2016-01-12 13:51:19 +0100

    fixed a bug with hero_picture

--- src/handleInput.c
+++ src/handleInput.c
@@ -306,6 +306,7 @@ void examine(void)
                 //examined item IS in the current room
                 player.position.y = 32;
                 player.position.x = 7;
+                printf("%s\n", field[32][7].ambience);
             }
 //------------------------------------------------------------------------------
             return;
